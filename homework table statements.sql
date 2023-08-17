1224d1a26ded01ac2024b7fc13a9f32777b1b306
Kevin-Lad <kevinladisheff@gmail.com>
2023-08-16 18:24:33 -0700

    final updates;

--- homework table statements.sql
+++ homework table statements.sql
@@ -1,24 +1,24 @@
 -- Create Statements
 DROP TABLE IF EXISTS departments CASCADE;
 CREATE TABLE departments(
-  dept_no VARCHAR PRIMARY KEY, 
-  dept_name VARCHAR
+  dept_no VARCHAR(4) PRIMARY KEY NOT NULL, 
+  dept_name VARCHAR(25)
 );
 
 DROP TABLE IF EXISTS titles CASCADE;
 CREATE TABLE titles(
-	title_id VARCHAR PRIMARY KEY,
-	title VARCHAR
+	title_id VARCHAR(5) PRIMARY KEY NOT NULL,
+	title VARCHAR(25)
 );
 
 DROP TABLE IF EXISTS employees CASCADE;
 CREATE TABLE employees(
-	emp_no INTEGER PRIMARY KEY,
-	emp_title_id VARCHAR,
+	emp_no INTEGER PRIMARY KEY NOT NULL,
+	emp_title_id VARCHAR(5),
 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
 	birth_date DATE,
-	first_name VARCHAR,
-	last_name VARCHAR,
+	first_name VARCHAR(15),
+	last_name VARCHAR(20),
 	sex CHAR(1),
 	hire_date DATE
 );
@@ -27,14 +27,14 @@ DROP TABLE IF EXISTS department_emp CASCADE;
 CREATE TABLE department_emp(
 	emp_no INTEGER,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	dept_no VARCHAR,
+	dept_no VARCHAR(4),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 
 DROP TABLE IF EXISTS department_mgr CASCADE;
 CREATE TABLE department_mgr(
-	dept_no VARCHAR,
+	dept_no VARCHAR(4),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	emp_no INTEGER,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
